2d038b03753c1efa03bf6fb704f8c1c3b00ef637
meloegel <meloegel@gmail.com>
2020-07-22 18:56:29 -0400

    trying something

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ server.use(helmet())
 server.use(express.json())
 server.use(cors())
 
+server.listen(3000, function () {
+    console.log('CORS-enabled web server listening on port 3000')
+})
+
 server.use('/auth', AuthRouter)
 server.use('/api/users', restrictedMiddleware, UserRouter)
 server.use('/api/questions', restrictedMiddleware, QuestionsRouter)

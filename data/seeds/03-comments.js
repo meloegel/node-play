bcbb3527c3dde01597c21499fd2fdfae6e3a5c70
meloegel <meloegel@gmail.com>
2020-07-29 12:42:22 -0400

    fixed seeds for deployment

--- data/seeds/03-comments.js
+++ data/seeds/03-comments.js
@@ -1,13 +1,10 @@
 
 exports.seed = function (knex, Promise) {
-  return knex('comments').truncate()
-    .then(function () {
-      return knex('comments').insert([
-        { id: 1, question_id: 1, user_id: 1, comment: 'Because its my favorite color' },
-        { id: 2, question_id: 1, user_id: 2, comment: 'I dont know' },
-        { id: 3, question_id: 2, user_id: 1, comment: '42' },
-        { id: 5, question_id: 2, user_id: 2, comment: 'I dont know' },
-        { id: 6, question_id: 3, user_id: 1, comment: 'No one knows' }
-      ]);
-    });
+  return knex('comments').insert([
+    { id: 1, question_id: 1, user_id: 1, comment: 'Because its my favorite color' },
+    { id: 2, question_id: 1, user_id: 2, comment: 'I dont know' },
+    { id: 3, question_id: 2, user_id: 1, comment: '42' },
+    { id: 5, question_id: 2, user_id: 2, comment: 'I dont know' },
+    { id: 6, question_id: 3, user_id: 1, comment: 'No one knows' }
+  ]);
 };
